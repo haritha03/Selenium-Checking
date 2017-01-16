5403d75cfd525d0f6879b39b2249f40db9a57d9c
haritha03 <haritharedys@gmail.com>
2017-01-16 15:29:58 -0500

    Added the POM XML and related files

---
+++ src/test/java/selenium/SearchPage.java
@@ -0,0 +1,15 @@
+package selenium;
+
+public interface SearchPage extends Page {
+    void search(String query);
+}
+
+
+/*FOR EXAMPLE NOW I ADD SOME CODE SNIPPET HERE..LIKE IF YOU WANT TO PRACTICE ON SOME REAL PROJECTS*/
+
+public interface LandingPage extends Landing {
+	void Search(String query )
+}
+
+//ACTUALLY WE CAN SEE THE OUTPUT OF THE APPLICATION BUT I AM NOT FAMILIAR ABOUT HOW THEY DO FOR SELENIUM.
+//WILL SHOW YOU SOME SAMPLE ONEk..FOR NOW LETS PUSH THIS CHANGE ok
